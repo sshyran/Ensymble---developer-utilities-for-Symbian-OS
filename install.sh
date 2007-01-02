c02e28d11f094c9feaba3e4f97673fccba505bc3
jethro <jethro@6fa842dc-9c34-11dd-ad9f-f77439c74b79>
2007-01-02 15:28:09 +0000

    Added signsis command for (re-)signing SIS files. Added altere32 command for altering pre-compiled Symbian EXEs and DLLs. Moved default certificate to its own module: defaultcert.py.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if test -z "$1"; then
-  print "install.sh targetpath"
+  echo "install.sh targetpath"
   exit 1
 fi
 
-squeeze/squeeze.py -1 -o ensymble -b cmdmain cmdmain.py cmd_py2sis.py cmd_version.py cryptutil.py miffile.py rscfile.py sisfield.py sisfile.py symbianutil.py
+squeeze/squeeze.py -1 -o ensymble -b cmdmain cmdmain.py cmd_altere32.py cmd_py2sis.py cmd_signsis.py cmd_version.py cryptutil.py miffile.py rscfile.py sisfield.py sisfile.py symbianutil.py defaultcert.py
 cp ensymble.py "$1"
 chmod +x "$1"/ensymble.py
 rm ensymble.py
