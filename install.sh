c54a8132bd7c353a0429e7a9a5003f39166127f5
jethro <jethro@6fa842dc-9c34-11dd-ad9f-f77439c74b79>
2008-01-27 18:48:16 +0000

    Made it possible to change the installation file name, also used in squeezing.

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 if test -z "$1"; then
-  echo "install.sh targetpath"
+  echo "install.sh targetpath [name]"
   exit 1
 fi
 
-squeeze/squeeze.py -1 -o ensymble -b cmdmain cmdmain.py \
+name=ensymble
+if test -n "$2"; then
+  name="`basename "$2" .py`"
+fi
+
+squeeze/squeeze.py -1 -o "$name" -b cmdmain cmdmain.py \
   cmd_altere32.py cmd_infoe32.py cmd_mergesis.py cmd_py2sis.py \
   cmd_signsis.py cmd_simplesis.py cmd_version.py cryptutil.py miffile.py \
   rscfile.py sisfield.py sisfile.py symbianutil.py defaultcert.py
-cp ensymble.py "$1"
-chmod +x "$1"/ensymble.py
-rm ensymble.py
+cp "$name".py "$1"
+chmod +x "$1"/"$name".py
+rm "$name".py
 
-echo "Ensymble command line tool installed as $1/ensymble.py"
+echo "Ensymble command line tool installed as $1/$name.py"
